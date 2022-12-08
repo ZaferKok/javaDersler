258b570ea8951afc8c91d800ca8a1a7d24924f2a
ZaferKok <zaferkok111@gmail.com>
2022-12-08 14:38:46 +0100

    isim düzeltmeleri yapıldı

--- javaDersler/src/odevJava2/PrimitiveDataTypes.java
+++ javaDersler/src/odevJava2/PrimitiveDataTypes.java
@@ -1,10 +1,10 @@
 package odevJava2;
 
-public class Odev3 {
+public class PrimitiveDataTypes {
 
 	public static void main(String[] args) {
 
-		System.out.println("Odev 3");
+		System.out.println("Odev 2");
 
 	}
 
