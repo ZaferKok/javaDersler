d2cb0452369a9d732c0888834bfc2bc233005750
ZaferKok <zaferkok111@gmail.com>
2022-12-08 14:36:01 +0100

    ikinci ders java odevleri tamamalandi

---
+++ javaDersler/src/odevJava2/Odev2.java
@@ -0,0 +1,10 @@
+package odevJava2;
+
+public class Odev2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaDersler/src/odevJava2/Odev3.java
@@ -0,0 +1,11 @@
+package odevJava2;
+
+public class Odev3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Odev 3");
+
+	}
+
+}
